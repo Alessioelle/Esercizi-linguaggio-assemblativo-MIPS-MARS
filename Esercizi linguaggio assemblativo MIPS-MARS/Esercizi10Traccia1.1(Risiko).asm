89c6007b0a24e39a8346210443558b0464cca78a
Alessio <alessiol@protonmail.com>
2026-02-09 19:30:00 +0100

    Update Esercizi10Traccia1.1(Risiko).asm

--- Esercizi linguaggio assemblativo MIPS-MARS/Esercizi10Traccia1.1(Risiko).asm
+++ Esercizi linguaggio assemblativo MIPS-MARS/Esercizi10Traccia1.1(Risiko).asm
@@ -4,17 +4,17 @@
 # Il programma poi con una funzione ordina i 3 numeri dell'attacco e con la stessa funzione ordina i 3 numeri della difesa. 
 # Poi con una ulteriore funzione valuta i carri armarti persi dalla difesa e quelli persi dall'attacco li sottrae alle truppe correnti e si prosegue. 
 # Il gioco termina quando la difesa arriva a 0 (Vittoria Attacco) o quando l'attacco ha solo 3 carri armati (Vittoria Difesa).
-# N.B. il programma effettuer· il lancio di 3 dadi indipendentemente dal numero di carri rimasti all'attaccante o al difensore se ancora in partita)
-# N.B. il programma È stato scritto in modo da non utilizzare lo stack)
+# N.B. il programma effettuer√° il lancio di 3 dadi indipendentemente dal numero di carri rimasti all'attaccante o al difensore se ancora in partita
+# N.B. il programma √© stato scritto in modo da non utilizzare lo stack
 
 .globl main
 
 .data
 
 carriblu: .asciiz "Insersca il numero di carri blu in attacco:"
-carrigialli: .asciiz "Inserisca il numero di carri gialli in difesa"
-vinceblu: .asciiz "Il vincitore della battaglia È il giocatore blu in attacco"
-vincegiallo: .asciiz "Il vincitore della battaglia È il giocatore giallo in difesa"
+carrigialli: .asciiz "Inserisca il numero di carri gialli in difesa:"
+vinceblu: .asciiz "Il vincitore della battaglia √© il giocatore blu in attacco"
+vincegiallo: .asciiz "Il vincitore della battaglia √© il giocatore giallo in difesa"
 acapo: .asciiz "\n"
 vettoreattacco: .word 1:3
 vettoredifesa: .word 1:3
@@ -33,36 +33,45 @@ li $v0,4                                 #spazio a capo
 la $a0,acapo
 syscall
 li $v0,4
-la $a0,carrigialli                       #Inserisco numero di carri gialli e lo salvo nel registro $t0 (difesa)
+la $a0,carrigialli                       #Inserisco numero di carri gialli e lo salvo nel registro $t1 (difesa)
 syscall
 li $v0,5
 syscall
 move $t1,$v0
 
-lanciodadi:                              #Genero 3 numeri casuali compresi tra 0 e 6 che simulano il lancio di tre dadi per l'attacco e li metto in vettoreattacco ordinati
+lanciodadi:                              #Genero 3 numeri casuali compresi tra 1 e 6 che simulano il lancio di tre dadi per l'attacco e li metto in vettoreattacco ordinati
 li $t3,6                            
 move $a1,$t3
+lancioprimodado:
 li $v0,42                                
 syscall
-sw $a0,vettoreattacco                     
+beqz $a0,lancioprimodado
+sw $a0,vettoreattacco
+lanciosecondodado:                     
 li $v0,42                                
 syscall
+beqz $a0,lanciosecondodado
 sw $a0,vettoreattacco+4
-add $t2,$t2,4
+lancioterzodado:
 li $v0,42                                
 syscall
-sw $a0,vettoreattacco+8               
-la $t2,vettoredifesa                     #Genero 3 numeri casuali compresi tra 0 e 6 che simulano il lancio di tre dadi per la difesa e li metto in vettoredifesa ordinati
+beqz $a0,lancioterzodado
+sw $a0,vettoreattacco+8                 
 move $a1,$t3
+lancioquartodado:                        #Genero 3 numeri casuali compresi tra 1 e 6 che simulano il lancio di tre dadi per la difesa e li metto in vettoredifesa ordinati
 li $v0,42
+beqz $a0,lancioquartodado
 syscall
 sw $a0,vettoredifesa
+lancioquintodado:
 li $v0,42
 syscall
+beqz $a0,lancioquintodado
 sw $a0,vettoredifesa+4
-add $t2,$t2,4
+lanciosestodado:
 li $v0,42
 syscall
+beqz $a0,lanciosestodado
 sw $a0,vettoredifesa+8
 
                                          
